d71f7d5d97ef97cca5c97c330d1bd0de24d7670f
sbroadhurst <stepbro31@gmail.com>
2022-02-28 19:01:06 -0500

    start profile remake

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import { HashRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { createTheme, IconButton, ThemeProvider } from '@mui/material'
+import { Paper } from '@mui/material'
+import Brightness4Icon from '@mui/icons-material/Brightness4'
+import Brightness7Icon from '@mui/icons-material/Brightness7'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Footer from './components/Footer'
+import Header from './components/Header'
+
+import Home from './routes/home'
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation()
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return null
 }
 
-export default App;
+function App() {
+  const [darkMode, setDarkMode] = useState(true)
+
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? 'dark' : 'light',
+    },
+  })
+
+  return (
+    <ThemeProvider theme={theme}>
+      <HashRouter>
+        <Header />
+        <IconButton
+          sx={{
+            m: 1,
+            position: 'absolute',
+            right: 0,
+            top: 0,
+            color: theme.palette.mode === 'light' ? 'black' : 'white',
+          }}
+          onClick={() => {
+            setDarkMode(!darkMode)
+          }}>
+          {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+        </IconButton>
+        <ScrollToTop />
+        <Paper style={{ height: '100vh' }}>
+          <Routes>
+            {/* <Route path="/privacypolicy" component={PrivacyPolicy} />
+          <Route path="/termsofservice" component={TermsOfService} /> */}
+            <Route exact path="/" element={<Home />} />
+          </Routes>
+          <Footer />
+        </Paper>
+      </HashRouter>
+    </ThemeProvider>
+  )
+}
+
+export default App
